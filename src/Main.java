65dc65caf95d0cc98a92572f1f333bf8d6b92ad0
Gael Eiji Nogawa <gael.nogawa@gmail.com>
2023-06-28 21:42:10 -0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args)	{
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int valorA, valorB, soma;
+		
+		valorA = sc.nextInt();
+		valorB = sc.nextInt();
+		
+		soma = valorA + valorB;
+		
+		System.out.printf("Qual o primeiro valor: %d%n", valorA);
+		System.out.printf("Qual o segundo valor: %d%n", valorB);
+		System.out.printf("O valor total é de: %d%n", soma);
+		
+		
+		sc.close();
+	}
+
+}
