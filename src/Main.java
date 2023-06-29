7252b9d69e1eb2fc14482684e53dbdb497b47d09
Gael Eiji Nogawa <gael.nogawa@gmail.com>
2023-06-28 23:14:04 -0300

    Add: exercise 02

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
 	public static void main(String[] args)	{
 		
+		
+		/*
 		Scanner sc = new Scanner(System.in);
 		
 		int valorA, valorB, soma;
@@ -19,6 +21,22 @@ public class Main {
 		System.out.printf("O valor total é de: %d%n", soma);
 		
 		sc.close();
+		
+		*/
+		
+		//----------- Exercicio 02 ----------
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double area, raioQuad, pi;
+		
+		pi = 3.14159;
+		area = sc.nextDouble();
+		raioQuad = pi * (Math.pow(area, 2.00));
+		
+		System.out.printf("O valor da area é %.4f%n", area );
+		System.out.printf("O valor da raio ao quadrado * pi é : %.4f%n", raioQuad);
+		
 	}
 
 }
