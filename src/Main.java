ff2077eead7714ccdc8eb79fe532807282c66d32
Gael Eiji Nogawa <gael.nogawa@gmail.com>
2023-06-29 22:19:14 -0300

    Add: exercicio 3

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,13 @@ public class Main {
 		
 		
 		/*
+		------------ Exercicio 01 ----------- 
+		
+		 * Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+		 * mensagem explicativa, conforme exemplos.
+
+		 
+		 
 		Scanner sc = new Scanner(System.in);
 		
 		int valorA, valorB, soma;
@@ -22,9 +29,14 @@ public class Main {
 		
 		sc.close();
 		
-		*/
+		
 		
 		//----------- Exercicio 02 ----------
+		 *	Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+		 *	casas decimais conforme exemplos.
+		 *	Fórmula da área: area = π . raio 2
+		 *	Considere o valor de π = 3.14159 
+		 *
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -39,6 +51,34 @@ public class Main {
 		
 		
 		sc.close();
+	
+		----------- Exercicio 03 -----------
+		
+		* Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+		* de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+		*
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, C, D, dif;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		C = sc.nextInt();
+		D = sc.nextInt();
+		
+		dif = (A * B) - (C * D);
+		
+		System.out.printf("O valor de A e B: %d e %d %n", A, B);
+		System.out.printf("O valor de C e D: %d e %d %n", C, D);
+		System.out.printf("A diferença é: %d%n", dif);
+		
+		sc.close();
+		*/
+		
+		
+		
+		
 	}
 
 }
